330b52640880d4e58d95edb30e244a2a27905799
Esa Torkkila <esa.torkkila@gmail.com>
2026-06-05 16:48:02 +0300

    Add Users page and UsersList component

---
+++ Frontend/src/users/components/UserItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./UserItem.css";
+
+const UserItem = (props) => {
+  return <div>UserItem</div>;
+};
+
+UserItem.propTypes = {};
+
+export default UserItem;
--- Frontend/src/users/pages/Users.js
+++ Frontend/src/users/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import UserList from "../components/UserList";
+
 const Users = () => {
-  return <div>Users</div>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Max Schwarz",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Max_Schwarzmueller.jpg/220px-Max_Schwarzmueller.jpg",
+      places: 3,
+    },
+  ];
+  return <UserList items={USERS} />;
 };
 
 export default Users;
