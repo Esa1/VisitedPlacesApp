840198af43577637365ab7eff70c51c9ae9f9324
Esa Torkkila <esa.torkkila@gmail.com>
2026-06-06 05:28:55 +0300

    Add UserItem component

--- Frontend/src/users/components/UserItem.js
+++ Frontend/src/users/components/UserItem.js
@@ -3,7 +3,21 @@ import React from "react";
 import "./UserItem.css";
 
 const UserItem = (props) => {
-  return <div>UserItem</div>;
+  return (
+    <li className="user-item">
+      <div className="user-item__content">
+        <div className="user-item__image">
+          <img src={props.image} alt={props.name} />
+        </div>
+      </div>
+      <div className="user-item__info">
+        <h2>{props.name}</h2>
+        <h3>
+          {props.placesCount} {props.placesCount === 1 ? "Place" : "Places"}
+        </h3>
+      </div>
+    </li>
+  );
 };
 
 UserItem.propTypes = {};
--- Frontend/src/users/pages/Users.js
+++ Frontend/src/users/pages/Users.js
@@ -8,7 +8,7 @@ const Users = () => {
       id: "u1",
       name: "Max Schwarz",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Max_Schwarzmueller.jpg/220px-Max_Schwarzmueller.jpg",
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=100&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyc3xlbnwwfHwwfHx8MA%3D%3D",
       places: 3,
     },
   ];
