8b2e85a7e8cdc02499e230f82bdbafce16e8e3cc
Esa Torkkila <esa.torkkila@gmail.com>
2026-06-06 11:36:21 +0300

    Add NavLinks to MainNavigation in MainHeader.

--- Frontend/src/shared/components/Navigation/NavLinks.js
+++ Frontend/src/shared/components/Navigation/NavLinks.js
@@ -1,8 +1,27 @@
 import React from "react";
-import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
+
+import "./NavLinks.css";
 
 const NavLinks = (props) => {
-  return <div>NavLinks</div>;
+  return (
+    <ul className="nav-links">
+      <li>
+        <NavLink to="/" exact>
+          All Users
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/u1/places">My Places</NavLink>
+      </li>
+      <li>
+        <NavLink to="/places/new">Add Place</NavLink>
+      </li>
+      <li>
+        <NavLink to="/auth">Authenticate</NavLink>
+      </li>
+    </ul>
+  );
 };
 
 NavLinks.propTypes = {};
