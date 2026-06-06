98c3229eb29c6d401fc8cb82c51dc26b40e245a3
Esa Torkkila <esa.torkkila@gmail.com>
2026-06-06 07:16:24 +0300

    Add the Main header

---
+++ Frontend/src/shared/components/Navigation/NavLinks.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const NavLinks = (props) => {
+  return <div>NavLinks</div>;
+};
+
+NavLinks.propTypes = {};
+
+export default NavLinks;
---
+++ Frontend/src/shared/components/Navigation/SideDrawer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const SideDrawer = (props) => {
+  return <div>SideDrawer</div>;
+};
+
+SideDrawer.propTypes = {};
+
+export default SideDrawer;
