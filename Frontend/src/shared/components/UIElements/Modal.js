16181f6836a49dc9cec8f4a03e04865a9958d1ea
Esa Torkkila <esa.torkkila@gmail.com>
2026-06-07 10:58:31 +0300

    Adding a Modal: use in PlaceItem component.

--- Frontend/src/shared/components/UIElements/Modal.js
+++ Frontend/src/shared/components/UIElements/Modal.js
@@ -1,60 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CSSTransition from "react-transition-group";
+import { CSSTransition } from "react-transition-group";
 
 import Backdrop from "./Backdrop";
 import "./Modal.css";
 
 const ModalOverlay = (props) => {
-    console.log("modal overlay");
-    console.log(props);
-    
-    const content = (
-        <div className={`modal ${props.className}`} style={props.style}>
-            <header className={`modal__header ${props.headerClass}`}>
-                <h2>{props.header}</h2>
-            </header>
-            <form
-                onSubmit={
-                    props.onSubmit ? props.onSubmit : (event) => event.preventDefault()
-                }
-            >
-                <div className={`modal__content ${props.contentClass}`}>
-                    {props.children}
-                </div>
-                <footer className={`modal__footer ${props.footerClass}`}>
-                    {props.footer}
-                </footer>
-            </form>
+  console.log("modal overlay");
+  console.log(props);
+
+  const content = (
+    <div className={`modal ${props.className}`} style={props.style}>
+      <header className={`modal__header ${props.headerClass}`}>
+        <h2>{props.header}</h2>
+      </header>
+      <form
+        onSubmit={
+          props.onSubmit ? props.onSubmit : (event) => event.preventDefault()
+        }
+      >
+        <div className={`modal__content ${props.contentClass}`}>
+          {props.children}
         </div>
-    );
-    return ReactDOM.createPortal(
-        content,
-        document.getElementById("modal-hook")
-    );
+        <footer className={`modal__footer ${props.footerClass}`}>
+          {props.footer}
+        </footer>
+      </form>
+    </div>
+  );
+  return ReactDOM.createPortal(content, document.getElementById("modal-hook"));
 };
 
 const Modal = (props) => {
-  return <>
-  {props.show && <Backdrop onClick={props.onCancel} />}
-    <CSSTransition 
+  console.log("modal");
+  console.log(props);
+
+  return (
+    <>
+      {props.show && <Backdrop onClick={props.onCancel} />}
+      <CSSTransition
         in={props.show}
         mountOnEnter
         unmountOnExit
         timeout={200}
         classNames="modal"
-    >
+      >
         <ModalOverlay {...props} />
-    </CSSTransition>
-  </>
-};
-
-Modal.propTypes = {};
-
-export default Modal;
-
-const Modal = (props) => {
-  return <div>Modal</div>;
+      </CSSTransition>
+    </>
+  );
 };
 
 Modal.propTypes = {};
