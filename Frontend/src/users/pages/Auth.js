30b1ef5a661cd06691b95037fdc14a706a40c5a9
Esa Torkkila <esa.torkkila@gmail.com>
2026-06-16 15:29:49 +0300

    Auth: finish login form.

--- Frontend/src/users/pages/Auth.js
+++ Frontend/src/users/pages/Auth.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { useForm } from "../../shared/hooks/form-hook";
 
+import Card from "../../shared/components/UIElements/Card";
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
 } from "../../shared/util/validators";
-import "../../places/pages/PlaceForm.css";
+import "./Auth.css";
 
 const Auth = () => {
   const [formState, inputHandler] = useForm(
@@ -30,38 +31,39 @@ const Auth = () => {
   };
 
   return (
-    <form className="place-form" onSubmit={authSubmitHandler}>
-      <div>
-        <label htmlFor="email">Email</label>
-        <Input
-          element="email"
-          type="email"
-          id="email"
-          placeholder="Email"
-          onChange={inputHandler}
-          value={formState.inputs.email.value}
-          validators={[VALIDATOR_EMAIL()]}
-          onInput={inputHandler}
-          errorText="Please enter a valid email address."
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <Input
-          type="password"
-          id="password"
-          placeholder="Password"
-          onChange={inputHandler}
-          value={formState.inputs.password.value}
-          validators={[VALIDATOR_MINLENGTH(6)]}
-          onInput={inputHandler}
-          errorText="Please enter a password with at least 6 characters."
-        />
-      </div>
-      <Button type="submit" disabled={!formState.isValid}>
-        Authenticate
-      </Button>
-    </form>
+    <Card className="authentication">
+      <h2>Login Required</h2>
+      <hr />
+      <form onSubmit={authSubmitHandler}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <Input
+            element="input"
+            id="email"
+            type="email"
+            label="E-mail"
+            validators={[VALIDATOR_EMAIL()]}
+            errorText="Please enter a valid email address."
+            onInput={inputHandler}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <Input
+            type="password"
+            element="input"
+            id="password"
+            label="Password"
+            validators={[VALIDATOR_MINLENGTH(6)]}
+            errorText="Please enter a password with at least 6 characters."
+            onInput={inputHandler}
+          />
+        </div>
+        <Button type="submit" disabled={!formState.isValid}>
+          LOGIN
+        </Button>
+      </form>
+    </Card>
   );
 };
 
