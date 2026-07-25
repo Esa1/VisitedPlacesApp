e1c35b2a7a84d58bbbbaf53adf47d3886cd79997
Esa Torkkila <esa.torkkila@gmail.com>
2026-07-25 10:50:09 +0300

    Use Google's Geoding API to convert an address into coordinates.

--- Backend/controllers/places-controllers.js
+++ Backend/controllers/places-controllers.js
@@ -3,6 +3,8 @@ const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
 
+const getCoordsForAddress = require("../util/location");
+
 const DUMMY_PLACES = [
   {
     id: "p1",
@@ -42,14 +44,24 @@ const getPlacesByUserId = (req, res, next) => {
   res.json({ places });
 };
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+    next(new HttpError("Invalid inputs passed, please check your data.", 422));
+    return;
   }
 
-  const { title, description, coordinates, address, creator } = req.body; // const title = req.body.title; const description = req.body.description; ...
+  const { title, description, address, creator } = req.body;
+
+  let coordinates;
+  try {
+    coordinates = await getCoordsForAddress(address);
+    console.log(coordinates); // { lat: 40.7484474, lng: -73.9871516 }
+  } catch (error) {
+    return next(error);
+  }
+
   const newPlace = {
     id: uuidv4(), // generates a unique id
     title,
---
+++ Backend/util/location.js
@@ -0,0 +1,32 @@
+const axios = require("axios");
+
+const HttpError = require("../models/http-error");
+
+const API_KEY = "8kOjeuOK2O-phrxaPjTeWuN1EMw=";
+
+async function getCoordsForAddress(address) {
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${API_KEY}`,
+  );
+
+  const data = response.data;
+
+  if (!data || data.status === "ZERO_RESULTS") {
+    const error = new HttpError(
+      "Could not find location for the specified address.",
+      422,
+    );
+    throw error;
+  }
+
+  console.log(data);
+  return data.results[0].geometry.location;
+}
+
+module.exports = getCoordsForAddress;
+
+//     lat: 40.7484474,
+//     lng: -73.9871516,
+//   }
+
+//}
