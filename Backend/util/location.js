288845736e191abcfaa2db8abb03c25c71dd09d3
Esa Torkkila <esa.torkkila@gmail.com>
2026-07-29 11:33:03 +0300

    Temporarily use dummy coordinates until getting new API key.

--- Backend/util/location.js
+++ Backend/util/location.js
@@ -5,28 +5,29 @@ const HttpError = require("../models/http-error");
 const API_KEY = "8kOjeuOK2O-phrxaPjTeWuN1EMw=";
 
 async function getCoordsForAddress(address) {
-  const response = await axios.get(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${API_KEY}`,
-  );
+  return {
+    lat: 40.7484474,
+    lng: -73.9871516,
+  };
 
-  const data = response.data;
+  // const response = await axios.get(
+  //   `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${API_KEY}`,
+  // );
 
-  if (!data || data.status === "ZERO_RESULTS") {
-    const error = new HttpError(
-      "Could not find location for the specified address.",
-      422,
-    );
-    throw error;
-  }
+  // const data = response.data;
 
-  console.log(data);
-  return data.results[0].geometry.location;
+  // if (!data || data.status === "ZERO_RESULTS") {
+  //   const error = new HttpError(
+  //     "Could not find location for the specified address.",
+  //     422,
+  //   );
+  //   throw error;
+  // }
+
+  // console.log(data);
+  // return data.results[0].geometry.location;
 }
 
 module.exports = getCoordsForAddress;
-
-//     lat: 40.7484474,
-//     lng: -73.9871516,
-//   }
 
 //}
