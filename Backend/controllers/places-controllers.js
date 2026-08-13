eb247ec19cc15970705c856aebb8d8e67c326db0
Esa Torkkila <esa.torkkila@gmail.com>
2026-08-13 10:01:54 +0300

    Clean up code.

--- Backend/controllers/places-controllers.js
+++ Backend/controllers/places-controllers.js
@@ -8,22 +8,8 @@ const getCoordsForAddress = require("../util/location");
 const Place = require("../models/place");
 const User = require("../models/user");
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584,
-    },
-    address: "20 W 34th St, New York, NY 10001",
-    creator: "u1",
-  },
-];
-
 const getPlaceById = async (req, res, next) => {
-  const placeId = req.params.pid; // { pid: 'p1' }
+  const placeId = req.params.pid;
 
   let place;
   try {
--- Backend/controllers/users-controllers.js
+++ Backend/controllers/users-controllers.js
@@ -1,17 +1,7 @@
-const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
-
-const DUMMY_USERS = [
-  {
-    id: "u1",
-    name: "John Doe",
-    email: "john@example.com",
-    password: "password123",
-  },
-];
 
 const getUserById = (req, res, next) => {
   const userId = req.params.uid;
