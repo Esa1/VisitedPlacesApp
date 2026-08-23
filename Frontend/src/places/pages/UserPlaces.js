f327e477177a28387d025e0bcf17e19a69323360
Esa Torkkila <esa.torkkila@gmail.com>
2026-08-23 11:21:50 +0300

    Fix and enhance loading places by user id.

--- Frontend/src/places/pages/UserPlaces.js
+++ Frontend/src/places/pages/UserPlaces.js
@@ -1,52 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import PlaceList from "../components/PlaceList";
 import { useHttpClient } from "../../shared/hooks/http-hook";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
-
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building 1",
-    description: "One of the most famous sky scrapers in the world!",
-    imageUrl:
-      "https://lh3.googleusercontent.com/gps-cs-s/APNQkAGNo50QB0M2EIP3qi32gH38lYiZ9OGsub1pFxX9-JUxLzGUlkutEctibOdBw7DZlDnKPHEjJURGCjo5cJRhDXy7vlWbS9nH1o55hlWe0YRNi2E8Tww2GlrBbQDiXc2-u5A6kvdGSQ=w270-h312-n-k-no",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584,
-    },
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "Empire State Building 2",
-    description: "One of the most famous sky scrapers in the world!",
-    imageUrl:
-      "https://lh3.googleusercontent.com/gps-cs-s/APNQkAGNo50QB0M2EIP3qi32gH38lYiZ9OGsub1pFxX9-JUxLzGUlkutEctibOdBw7DZlDnKPHEjJURGCjo5cJRhDXy7vlWbS9nH1o55hlWe0YRNi2E8Tww2GlrBbQDiXc2-u5A6kvdGSQ=w270-h312-n-k-no",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584,
-    },
-    creator: "u2",
-  },
-];
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 const UserPlaces = () => {
   const userId = useParams().userId;
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
-  try {
-    const loadedPlaces = sendRequest(
-      `http://localhost:5000/api/places/user/${userId}`,
-    );
-    return <PlaceList items={loadedPlaces} />;
-  } catch (err) {
-    console.error("Error fetching user places:", err);
-    return <ErrorModal error={err.message} onClear={clearError} />;
-  }
+  const [loadedPlaces, setLoadedPlaces] = useState();
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/places/user/${userId}`,
+        );
+        setLoadedPlaces(responseData.places);
+      } catch (err) {
+        // Error handling is already managed by the useHttpClient hook
+      }
+    };
+
+    fetchPlaces();
+  }, [sendRequest, userId]);
+
+  return (
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+    </>
+  );
 };
 
 UserPlaces.propTypes = {};
