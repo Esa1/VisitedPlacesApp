26e73d335786094e42fc407e5fc8e1d197a4af8c
Esa Torkkila <esa.torkkila@gmail.com>
2026-09-14 15:55:08 +0300

    Remove console.log.

--- Frontend/src/users/pages/Auth.js
+++ Frontend/src/users/pages/Auth.js
@@ -69,7 +69,6 @@ const Auth = () => {
   const authSubmitHandler = async (event) => {
     event.preventDefault();
 
-    console.log("Form State:", formState.inputs); // Log the form state for debugging
     if (isLoginMode) {
       try {
         const responseData = await sendRequest(
